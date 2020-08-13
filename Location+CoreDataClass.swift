2aa3938456b6b63993fc58f37a02107ed115eadb
MelissaGarrett <melissa_garrett@hotmail.com>
2020-08-13 15:16:03 -0500

    Created the data model for use with Core Data.

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Melissa  Garrett on 8/13/20.
+//  Copyright © 2020 MelissaGarrett. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
